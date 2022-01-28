1b97adfa418897de6ac37b3212228b238566814b
André Leão <andrefelipesl123@gmail.com>
2022-01-28 15:32:13 -0300

    Menu Aside finalizado

--- pokemon-marketplace/src/components/Aside/Aside.js
+++ pokemon-marketplace/src/components/Aside/Aside.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import * as S from './styled';
-import { MdHome } from 'react-icons/md';
+import { MdHome, MdPerson } from 'react-icons/md';
+import { FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+
+import logo from '../../assets/pokeapi_logo.png';
 
 function Aside() {
   return (
     <S.MenuContainer>
+      <S.LogoContainer>
+        <S.Logo src={logo} alt="pokeapi logo" />
+      </S.LogoContainer>
+
       <S.Menu>
         <S.MenuContent>
           <S.MenuItem>
-            <MdHome size={28} style={{ marginRight: '.2rem' }} />
-            <Link to="/">Home</Link>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+              <S.Content>
+                <MdHome size={32} style={{ marginRight: '.5rem' }} />
+                Home
+              </S.Content>
+            </Link>
+          </S.MenuItem>
+          <S.MenuItem>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+              <S.Content>
+                <FaShoppingCart size={25} style={{ marginRight: '.5rem' }} />
+                Poke Cart
+              </S.Content>
+            </Link>
+          </S.MenuItem>
+          <S.MenuItem>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+              <S.Content>
+                <MdPerson size={35} style={{ marginRight: '.5rem' }} />
+                Perfil
+              </S.Content>
+            </Link>
           </S.MenuItem>
         </S.MenuContent>
       </S.Menu>
--- pokemon-marketplace/src/components/Aside/styled.js
+++ pokemon-marketplace/src/components/Aside/styled.js
@@ -6,17 +6,66 @@ export const MenuContainer = styled.div`
   background-color: #f7f753;
 `;
 
+export const LogoContainer = styled.div`
+  margin-top: 12vh;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Logo = styled.img`
+  width: 10rem;
+  height: 4rem;
+`;
+
 export const Menu = styled.nav`
-  margin-top: 11vh;
+  margin-top: 5.5vh;
+  display: flex;
+  justify-content: center;
 `;
 
 export const MenuContent = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   text-decoration: none;
-  padding: 1rem 3rem;
+  padding: 0.5rem 1rem;
+  background-color: #f2fd00;
+  padding: 1rem 0;
+  border-radius: 1rem;
+  width: 86%;
+  box-shadow: 0px 2px 7px -1px rgba(0, 0, 0, 0.5);
 `;
 
 export const MenuItem = styled.li`
   display: flex;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   font-weight: bold;
+  align-items: center;
+  transition: all 0.4s;
+  margin-bottom: 0.8rem;
+
+  &:hover {
+    background-color: #f8fbae;
+    border-radius: 10px;
+    box-shadow: 0px 10px 7px -1px rgba(0, 0, 0, 0.5);
+    transform: translateY(-4px);
+  }
+
+  &:active {
+    box-shadow: 0px 5px 7px -1px rgba(0, 0, 0, 0.5);
+    background-color: #f2fd00;
+  }
+`;
+
+export const Content = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #000;
+  cursor: pointer;
+  padding: 0.6rem 1rem;
+
+  &:active {
+    color: #001ffd;
+  }
 `;
