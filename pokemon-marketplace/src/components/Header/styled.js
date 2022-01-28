6be01e49323bccc7e362353063d52cf555bc7b26
André Leão <andrefelipesl123@gmail.com>
2022-01-27 23:18:59 -0300

    Adicao da background-image ao Header

--- pokemon-marketplace/src/components/Header/styled.js
+++ pokemon-marketplace/src/components/Header/styled.js
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
+import faixa from '../../assets/faixa.jpg';
 
 export const HeaderBar = styled.header`
   grid-area: MH;
-  height: 13.6vh;
-  background-color: red;
+  height: 18vh;
   box-shadow: 0px 0px 4px 2px rgba(0, 0, 0, 0.81);
   z-index: 1;
+  background-image: linear-gradient(
+      180deg,
+      rgba(245, 255, 0, 0.7) 0%,
+      rgba(215, 206, 23, 0.6) 100%
+    ),
+    url(${faixa});
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 85rem;
 `;
