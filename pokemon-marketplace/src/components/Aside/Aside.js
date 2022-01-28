f087d68aecb7f78f5b1e9644fa7cb01bc6a1d1f6
André Leão <andrefelipesl123@gmail.com>
2022-01-28 10:55:17 -0300

    Updated Aside e Header

--- pokemon-marketplace/src/components/Aside/Aside.js
+++ pokemon-marketplace/src/components/Aside/Aside.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import * as S from './styled';
+import { MdHome } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 function Aside() {
-  return <S.MenuContainer></S.MenuContainer>;
+  return (
+    <S.MenuContainer>
+      <S.Menu>
+        <S.MenuContent>
+          <S.MenuItem>
+            <MdHome size={28} style={{ marginRight: '.2rem' }} />
+            Home
+          </S.MenuItem>
+        </S.MenuContent>
+      </S.Menu>
+    </S.MenuContainer>
+  );
 }
 
 export default Aside;
--- pokemon-marketplace/src/components/Aside/styled.js
+++ pokemon-marketplace/src/components/Aside/styled.js
@@ -3,5 +3,20 @@ import styled from 'styled-components';
 export const MenuContainer = styled.div`
   grid-area: AS;
   height: 100vh;
-  background-color: #cecece;
+  background-color: #f7f753;
+`;
+
+export const Menu = styled.nav`
+  margin-top: 11vh;
+`;
+
+export const MenuContent = styled.ul`
+  text-decoration: none;
+  padding: 1rem 3rem;
+`;
+
+export const MenuItem = styled.li`
+  display: flex;
+  font-size: 1.5rem;
+  font-weight: bold;
 `;
