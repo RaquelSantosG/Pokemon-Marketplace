813b45f6752e715cca2df7417fb6b30ae2778e4c
raquelnew <raquelnew3@gmail.com>
2022-01-29 12:03:57 -0300

    primeira-parte-home

--- pokemon-marketplace/src/components/Home/home.js
+++ pokemon-marketplace/src/components/Home/home.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import * as S from './styled';
-import logo from '../../assets/pikachu.png';
+import pikachu from '../../assets/pikachu.png';
+import squirtle from '../../assets/squirtle.png';
+import borboleta from '../../assets/borboleta.png';
+import Charmander from '../../assets/Charmander.png';
+import Ivysaur from '../../assets/Ivysaur.png';
+import Ninetales from '../../assets/Ninetales.png';
+import Bulbasaur from '../../assets/Bulbasaur.png';
+import Palkia from '../../assets/palkia.png';
+
 
 
 
 
 function Home() {
-  const pokemons = [
-    { id: 1, nome: 'Pikachu', tipo: 'Elétrico', preco: 'R$ 20.000,00' },
-    { id: 2, nome: 'Bulbasaur', tipo: 'Natureza', preco: 'R$ 45.000,00' },
-    { id: 3, nome: 'Charmander', tipo: 'Fogo', preco: 'R$ 30.000,00' },
-    { id: 4, nome: 'Squirtle', tipo: 'Água', preco: 'R$ 35.000,00' },
-    { id: 5, nome: 'Blastoise', tipo: 'Elétrico', preco: 'R$ 45.000,00' },
-    { id: 6, nome: 'Ivysaur ', tipo: 'Elétrico', preco: 'R$ 55.000,00' },
-    { id: 7, nome: 'Ninetales', tipo: 'Elétrico', preco: 'R$ 25.000,00' },
-    { id: 8, nome: 'Butterfree ', tipo: 'Elétrico', preco: 'R$ 23.000,00' },
-  ];
+  let pokemons = [
+    {id: 1, img: pikachu,  nome: 'Pikachu', tipo: 'Elétrico', preco: 'R$ 20.000,00'},
+    { id: 2,img: Charmander, nome: 'Charmander', tipo: 'Fogo', preco: 'R$ 30.000,00' },
+    { id: 3,img: squirtle, nome: 'squirtle', tipo: 'Água', preco: 'R$ 35.000,00' },
+    { id: 4,img:Bulbasaur, nome: 'Bulbasaur', tipo: 'Grama/veneno', preco: 'R$ 35.000,00' },
+    { id: 5, img:Palkia, nome: 'Palkia', tipo: 'Lendário', preco: 'R$ 260.000,00',},
 
+  ];
+ 
   return (
     <S.Container>
       <S.Lista>
         {pokemons.map((pokemon) => (
           <S.Item key={pokemon.id}>
             <S.Card>
-              <S.Imagem src={logo} alt="foto pokemon" />
+              <S.Imagem src={pokemon.img} alt="foto pokemon" />
               <S.Title>{pokemon.nome}</S.Title>
               <S.Tipo>{pokemon.tipo}</S.Tipo>
               <S.Detalhes>+ detalhes</S.Detalhes>
               <S.Preco>{pokemon.preco}</S.Preco>
               <S.Carrinho>Adicionar ao carrinho</S.Carrinho>
             </S.Card>
+            
           </S.Item>
         ))}
       </S.Lista>
